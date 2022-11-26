b2386902db5009ccd1ce92e273911676565d1a55
Rafael Pereira <rafael.pereira@liferay.com>
2022-11-26 14:54:55 -0300

    Add some Challenges

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -101,7 +101,7 @@ export function Video(props:VideoProps) {
 
 
                         <a
-                            href=""
+                            href="https://forms.gle/tn18Z7tRcPnqdRwW6"
                             className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 mt-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors"
                         >
                             <Lightning size={24} />
